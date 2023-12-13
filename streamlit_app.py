cc259d91674a06174da5c2f114c937127ecec5d8
ayushpuhaniitm <153740961+ayushpuhaniitm@users.noreply.github.com>
2023-12-13 19:46:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ def find_largest (num1, num2, num3):
 
 st.title('Find the Largest Number')
 
+st.image("Screenshot 2023-12-13 194430.png", use_column_width=True)
+
 num1 = st.number_input('Enter first number', value=0.0) 
 num2 = st.number_input('Enter second number', value=0.0) 
 num3 = st.number_input('Enter third number', value=0.0)
