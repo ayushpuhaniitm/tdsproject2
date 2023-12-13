92809154a6d137d7039a29c76045fc02161c940d
ayushpuhaniitm <153740961+ayushpuhaniitm@users.noreply.github.com>
2023-12-13 20:02:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ num1 = st.number_input('Enter first number', value=0.0)
 num2 = st.number_input('Enter second number', value=0.0) 
 num3 = st.number_input('Enter third number', value=0.0)
 
-if st.button('Find the largest number'):
+if st.button('Find Largest'):
     largest = find_largest (num1, num2, num3) 
     st.success(f'The largest number is: {largest}')
